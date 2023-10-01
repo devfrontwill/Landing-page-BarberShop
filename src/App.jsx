6466ad59047b6564dfc48864429440d00b835b5e
Wiliam Roza <devfrontwill@gmail.com>
2023-10-01 01:44:39 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header"
+
+function App() {
+  return (
+    <>
+      <div>
+        <Header/>
+        <h1>Hello World</h1>
+      </div>
+    </>
+
+  )
+}
+
+export default App
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.scss';
+
+export default function Header(){
+    return(
+        <div>
+            <h1 className={styles.container} >HEADER</h1>
+        </div>
+    )
+}
